0d2c920f5770cf11ad943a49ee8d64b2c0bc421a
colenhyt <colen.hyt@gmail.com>
2016-12-11 19:11:54 +0800

    add

---
+++ backend/views/game/index.php
@@ -0,0 +1,415 @@
+
+<?php
+use yii\widgets\LinkPager;
+use yii\base\Object;
+use yii\bootstrap\ActiveForm;
+use common\utils\CommonFun;
+use yii\helpers\Url;
+
+use common\models\Game;
+
+$modelLabel = new \common\models\Game();
+?>
+
+<?php $this->beginBlock('header');  ?>
+<!-- <head></head>中代码块 -->
+<?php $this->endBlock(); ?>
+
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+      
+        <div class="box-header">
+          <h3 class="box-title">数据列表</h3>
+          <div class="box-tools">
+            <div class="input-group input-group-sm" style="width: 150px;">
+                <button id="create_btn" type="button" class="btn btn-xs btn-primary">添&nbsp;&emsp;加</button>
+        			|
+        		<button id="delete_btn" type="button" class="btn btn-xs btn-danger">批量删除</button>
+            </div>
+          </div>
+        </div>
+        <!-- /.box-header -->
+        
+        <div class="box-body">
+          <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+            <!-- row start search-->
+          	<div class="row">
+          	<div class="col-sm-12">
+                <?php ActiveForm::begin(['id' => 'game-search-form', 'method'=>'get', 'options' => ['class' => 'form-inline'], 'action'=>Url::toRoute('game/index')]); ?>     
+                
+                  <div class="form-group" style="margin: 5px;">
+                      <label><?=$modelLabel->getAttributeLabel('game_id')?>:</label>
+                      <input type="text" class="form-control" id="query[game_id]" name="query[game_id]"  value="<?=isset($query["game_id"]) ? $query["game_id"] : "" ?>">
+                  </div>
+              <div class="form-group">
+              	<a onclick="searchAction()" class="btn btn-primary btn-sm" href="#"> <i class="glyphicon glyphicon-zoom-in icon-white"></i>搜索</a>
+           	  </div>
+               <?php ActiveForm::end(); ?> 
+            </div>
+          	</div>
+          	<!-- row end search -->
+          	
+          	<!-- row start -->
+          	<div class="row">
+          	<div class="col-sm-12">
+          	<table id="data_table" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="data_table_info">
+            <thead>
+            <tr role="row">
+            
+            <?php 
+              $orderby = isset($_GET['orderby']) ? $_GET['orderby'] : '';
+		      echo '<th><input id="data_table_check" type="checkbox"></th>';
+              echo '<th onclick="orderby(\'game_id\', \'desc\')" '.CommonFun::sortClass($orderby, 'game_id').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('game_id').'</th>';
+              echo '<th onclick="orderby(\'name\', \'desc\')" '.CommonFun::sortClass($orderby, 'name').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('name').'</th>';
+              echo '<th onclick="orderby(\'cdesc\', \'desc\')" '.CommonFun::sortClass($orderby, 'cdesc').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('cdesc').'</th>';
+              echo '<th onclick="orderby(\'icon\', \'desc\')" '.CommonFun::sortClass($orderby, 'icon').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('icon').'</th>';
+              echo '<th onclick="orderby(\'status\', \'desc\')" '.CommonFun::sortClass($orderby, 'status').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('status').'</th>';
+              echo '<th onclick="orderby(\'remark\', \'desc\')" '.CommonFun::sortClass($orderby, 'remark').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('remark').'</th>';
+              echo '<th onclick="orderby(\'createtime\', \'desc\')" '.CommonFun::sortClass($orderby, 'createtime').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('createtime').'</th>';
+         
+			?>
+	
+            <th tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >操作</th>
+            </tr>
+            </thead>
+            <tbody>
+            
+            <?php
+            foreach ($models as $model) {
+                echo '<tr id="rowid_' . $model->game_id . '">';
+                echo '  <td><label><input type="checkbox" value="' . $model->game_id . '"></label></td>';
+                echo '  <td>' . $model->game_id . '</td>';
+                echo '  <td>' . $model->name . '</td>';
+                echo '  <td>' . $model->cdesc . '</td>';
+                echo '  <td>' . $model->icon . '</td>';
+                echo '  <td>' . $model->status . '</td>';
+                echo '  <td>' . $model->remark . '</td>';
+                echo '  <td>' . $model->createtime . '</td>';
+                echo '  <td class="center">';
+                echo '      <a id="view_btn" onclick="viewAction(' . $model->game_id . ')" class="btn btn-primary btn-sm" href="#"> <i class="glyphicon glyphicon-zoom-in icon-white"></i>查看</a>';
+                echo '      <a id="edit_btn" onclick="editAction(' . $model->game_id . ')" class="btn btn-primary btn-sm" href="#"> <i class="glyphicon glyphicon-edit icon-white"></i>修改</a>';
+                echo '      <a id="delete_btn" onclick="deleteAction(' . $model->game_id . ')" class="btn btn-danger btn-sm" href="#"> <i class="glyphicon glyphicon-trash icon-white"></i>删除</a>';
+                echo '  </td>';
+                echo '</tr>';
+            }
+            
+            ?>
+            
+           
+           
+            </tbody>
+            <!-- <tfoot></tfoot> -->
+          </table>
+          </div>
+          </div>
+          <!-- row end -->
+          
+          <!-- row start -->
+          <div class="row">
+          	<div class="col-sm-5">
+            	<div class="dataTables_info" id="data_table_info" role="status" aria-live="polite">
+            		<div class="infos">
+            		从<?= $pages->getPage() * $pages->getPageSize() + 1 ?>            		到 <?= ($pageCount = ($pages->getPage() + 1) * $pages->getPageSize()) < $pages->totalCount ?  $pageCount : $pages->totalCount?>            		 共 <?= $pages->totalCount?> 条记录</div>
+            	</div>
+            </div>
+          	<div class="col-sm-7">
+              	<div class="dataTables_paginate paging_simple_numbers" id="data_table_paginate">
+              	<?= LinkPager::widget([
+              	    'pagination' => $pages,
+              	    'nextPageLabel' => '»',
+              	    'prevPageLabel' => '«',
+              	    'firstPageLabel' => '首页',
+              	    'lastPageLabel' => '尾页',
+              	]); ?>	
+              	
+              	</div>
+          	</div>
+		  </div>
+		  <!-- row end -->
+        </div>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+    </div>
+    <!-- /.col -->
+  </div>
+  <!-- /.row -->
+</section>
+<!-- /.content -->
+
+<div class="modal fade" id="edit_dialog" tabindex="-1" role="dialog"
+	aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">×</button>
+				<h3>Settings</h3>
+			</div>
+			<div class="modal-body">
+                <?php $form = ActiveForm::begin(["id" => "game-form", "class"=>"form-horizontal", "action"=>Url::toRoute("game/save")]); ?>                      
+                 
+          <input type="hidden" class="form-control" id="game_id" name="game_id" />
+
+          <div id="name_div" class="form-group">
+              <label for="name" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("name")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="name" name="Game[name]" placeholder="" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="cdesc_div" class="form-group">
+              <label for="cdesc" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("cdesc")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="cdesc" name="Game[cdesc]" placeholder="" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="icon_div" class="form-group">
+              <label for="icon" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("icon")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="icon" name="Game[icon]" placeholder="" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="status_div" class="form-group">
+              <label for="status" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("status")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="status" name="Game[status]" placeholder="" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="remark_div" class="form-group">
+              <label for="remark" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("remark")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="remark" name="Game[remark]" placeholder="" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="createtime_div" class="form-group">
+              <label for="createtime" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("createtime")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="createtime" name="Game[createtime]" placeholder="" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+                    
+
+			<?php ActiveForm::end(); ?>          
+                </div>
+			<div class="modal-footer">
+				<a href="#" class="btn btn-default" data-dismiss="modal">关闭</a> <a
+					id="edit_dialog_ok" href="#" class="btn btn-primary">确定</a>
+			</div>
+		</div>
+	</div>
+</div>
+<?php $this->beginBlock('footer');  ?>
+<!-- <body></body>后代码块 -->
+ <script>
+function orderby(field, op){
+	 var url = window.location.search;
+	 var optemp = field + " desc";
+	 if(url.indexOf('orderby') != -1){
+		 url = url.replace(/orderby=([^&?]*)/ig,  function($0, $1){ 
+			 var optemp = field + " desc";
+			 optemp = decodeURI($1) != optemp ? optemp : field + " asc";
+			 return "orderby=" + optemp;
+		   }); 
+	 }
+	 else{
+		 if(url.indexOf('?') != -1){
+			 url = url + "&orderby=" + encodeURI(optemp);
+		 }
+		 else{
+			 url = url + "?orderby=" + encodeURI(optemp);
+		 }
+	 }
+	 window.location.href=url; 
+ }
+ function searchAction(){
+		$('#game-search-form').submit();
+	}
+ function viewAction(id){
+		initModel(id, 'view', 'fun');
+	}
+
+ function initEditSystemModule(data, type){
+	if(type == 'create'){
+		$("#game_id").val('');
+		$("#name").val('');
+		$("#cdesc").val('');
+		$("#icon").val('');
+		$("#status").val('');
+		$("#remark").val('');
+		$("#createtime").val('');
+		
+	}
+	else{
+		$("#game_id").val(data.game_id);
+    	$("#name").val(data.name);
+    	$("#cdesc").val(data.cdesc);
+    	$("#icon").val(data.icon);
+    	$("#status").val(data.status);
+    	$("#remark").val(data.remark);
+    	$("#createtime").val(data.createtime);
+    	}
+	if(type == "view"){
+      $("#game_id").attr({readonly:true,disabled:true});
+      $("#name").attr({readonly:true,disabled:true});
+      $("#cdesc").attr({readonly:true,disabled:true});
+      $("#icon").attr({readonly:true,disabled:true});
+      $("#status").attr({readonly:true,disabled:true});
+      $("#remark").attr({readonly:true,disabled:true});
+      $("#createtime").attr({readonly:true,disabled:true});
+	$('#edit_dialog_ok').addClass('hidden');
+	}
+	else{
+      $("#game_id").attr({readonly:false,disabled:false});
+      $("#name").attr({readonly:false,disabled:false});
+      $("#cdesc").attr({readonly:false,disabled:false});
+      $("#icon").attr({readonly:false,disabled:false});
+      $("#status").attr({readonly:false,disabled:false});
+      $("#remark").attr({readonly:false,disabled:false});
+      $("#createtime").attr({readonly:false,disabled:false});
+		$('#edit_dialog_ok').removeClass('hidden');
+		}
+		$('#edit_dialog').modal('show');
+}
+
+function initModel(id, type, fun){
+	
+	$.ajax({
+		   type: "GET",
+		   url: "<?=Url::toRoute('game/view')?>",
+		   data: {"id":id},
+		   cache: false,
+		   dataType:"json",
+		   error: function (xmlHttpRequest, textStatus, errorThrown) {
+			    alert("出错了，" + textStatus);
+			},
+		   success: function(data){
+			   initEditSystemModule(data, type);
+		   }
+		});
+}
+	
+function editAction(id){
+	initModel(id, 'edit');
+}
+
+function deleteAction(id){
+	var ids = [];
+	if(!!id == true){
+		ids[0] = id;
+	}
+	else{
+		var checkboxs = $('#data_table :checked');
+	    if(checkboxs.size() > 0){
+	        var c = 0;
+	        for(i = 0; i < checkboxs.size(); i++){
+	            var id = checkboxs.eq(i).val();
+	            if(id != ""){
+	            	ids[c++] = id;
+	            }
+	        }
+	    }
+	}
+	if(ids.length > 0){
+		admin_tool.confirm('请确认是否删除', function(){
+		    $.ajax({
+				   type: "GET",
+				   url: "<?=Url::toRoute('game/delete')?>",
+				   data: {"ids":ids},
+				   cache: false,
+				   dataType:"json",
+				   error: function (xmlHttpRequest, textStatus, errorThrown) {
+					    admin_tool.alert('msg_info', '出错了，' + textStatus, 'warning');
+					},
+				   success: function(data){
+					   for(i = 0; i < ids.length; i++){
+						   $('#rowid_' + ids[i]).remove();
+					   }
+					   admin_tool.alert('msg_info', '删除成功', 'success');
+					   window.location.reload();
+				   }
+				});
+		});
+	}
+	else{
+		admin_tool.alert('msg_info', '请先选择要删除的数据', 'warning');
+	}
+    
+}
+
+function getSelectedIdValues(formId)
+{
+	var value="";
+	$( formId + " :checked").each(function(i)
+	{
+		if(!this.checked)
+		{
+			return true;
+		}
+		value += this.value;
+		if(i != $("input[name='game_id']").size()-1)
+		{
+			value += ",";
+		}
+	 });
+	return value;
+}
+
+$('#edit_dialog_ok').click(function (e) {
+    e.preventDefault();
+	$('#game-form').submit();
+});
+
+$('#create_btn').click(function (e) {
+    e.preventDefault();
+    initEditSystemModule({}, 'create');
+});
+
+$('#delete_btn').click(function (e) {
+    e.preventDefault();
+    deleteAction('');
+});
+
+$('#game-form').bind('submit', function(e) {
+	e.preventDefault();
+	var id = $("#game_id").val();
+	var action = id == "" ? "<?=Url::toRoute('game/create')?>" : "<?=Url::toRoute('game/update')?>";
+    $(this).ajaxSubmit({
+    	type: "post",
+    	dataType:"json",
+    	url: action,
+    	success: function(value) 
+    	{
+        	if(value.errno == 0){
+        		$('#edit_dialog').modal('hide');
+        		admin_tool.alert('msg_info', '添加成功', 'success');
+        		window.location.reload();
+        	}
+        	else{
+            	var json = value.data;
+        		for(var key in json){
+        			$('#' + key).attr({'data-placement':'bottom', 'data-content':json[key], 'data-toggle':'popover'}).addClass('popover-show').popover('show');
+        			
+        		}
+        	}
+
+    	}
+    });
+});
+
+ 
+</script>
+<?php $this->endBlock(); ?>
